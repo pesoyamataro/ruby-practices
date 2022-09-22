2e25cc31fcd82fa0004a2f37823b6038e9138aea
pesoyama <pesoyama.taro@gmail.com>
2022-09-22 19:16:08 +0900

    コメント指摘を受けて.to_s箇所を変更

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizz_buzz(n)
   result = ""
   result = "Fizz" if n % 3 == 0
   result += "Buzz" if n % 5 == 0
-  result = n if result.empty?
-  result.to_s
+  result = n.to_s if result.empty?
+  result
 end
 
 (1..20).each do |i|
