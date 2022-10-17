13af508f7861e7562080c83a7c011d668492eb5d
pesoyama <pesoyama.taro@gmail.com>
2022-10-17 17:30:17 +0900

    Post-review fix

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -3,56 +3,57 @@
 
 require 'optparse'
 
-max_column = 3
-space_length = 3
-items = []
+MAX_COLUMN = 3
+SPACE_LENGTH = 3
 
-# ターゲットの取得
-def all_files(items)
-  if ARGV.empty? # 引数なしならカレントディレクトリを取得
+def main
+  files = search_files
+  row_count = (files.size.to_f / MAX_COLUMN).ceil
+  display_width = ajust_width(files).max + SPACE_LENGTH
+  output(row_count, files, display_width)
+end
+
+def search_files
+  files = []
+  if ARGV.empty?
     dirpath = Dir.getwd
-  elsif Dir.exist?(ARGV[0]) # 引数がディレクトリパスとして認識されるならそのまま取得
+  elsif Dir.exist?(ARGV[0])
     dirpath = ARGV[0]
-  elsif FileTest.exist?(ARGV[0]) # 引数がファイルとして認識ならそのまま取得
-    items << ARGV[0]
-  else
-    puts "lsrb: '#{ARGV[0]}' にアクセスできません：そのようなファイルやディレクトリはありません"
-    exit
+  elsif FileTest.exist?(ARGV[0])
+    files << ARGV[0]
   end
 
-  # ディレクトリ指定であれば配列に変換&隠しファイル除去
-  if items.empty?
-    Dir.foreach(dirpath) do |item|
-      items << item unless /^\./.match?(item)
+  if files.empty?
+    Dir.foreach(dirpath) do |file|
+      files << file unless file.start_with?('.')
     end
   end
-  items.sort
+  files.sort
 end
 
-# 出力
-def output(row, max_column, items, file_width)
-  row.times do |m|
-    max_column.times do |n|
-      print ljust_kana(items[m + n * row], file_width) unless items[m + n * row].nil?
-      print "\n" if ((n + 1) % max_column).zero?
+def output(max_row, files_name, frame_width)
+  max_row.times do |i|
+    MAX_COLUMN.times do |j|
+      print ljust_kana(files_name[i + j * max_row], frame_width) unless files_name[i + j * max_row].nil?
+      print "\n" if ((j + 1) % MAX_COLUMN).zero?
     end
   end
 end
 
-# ファイル文字数＋全角（アスキーコードではない）文字の場合は1文字分追加
-def length_chk(items)
-  items.map do |x|
-    x.length + x.chars.count { |num| !num.ascii_only? }
+def ajust_width(files_str)
+  addition_count = 2
+  files_str.map do |file_str|
+    judge_ascii(file_str, addition_count)
   end
 end
 
-# 全角の文字数分が右にずれてしまうので全角文字数分マイナスして左詰め
-def ljust_kana(str, file_width)
-  str.ljust(file_width - str.chars.count { |num| !num.ascii_only? })
+def ljust_kana(file_name, max_width)
+  addition_count = 1
+  file_name.ljust(max_width - judge_ascii(file_name, addition_count))
 end
 
-# options = ARGV.getopts('arl') # 以降の課題用にオプション枠だけ作成。使っていないとrubocopにしょっぴかれるので使うまでコメントアウト
-items = all_files(items) # 指定パス（引数なしならカレントディレクトリ）配下の情報を取得
-row = (items.size.to_f / max_column).ceil # 表示する行数を取得
-file_width = length_chk(items).max + space_length # 表示文字数の最大値取得+表示用スペース追加
-output(row, max_column, items, file_width) # 出力
+def judge_ascii(str, char_count)
+  str.chars.sum { |c| c.ascii_only? ? char_count - 1 : char_count }
+end
+
+main
