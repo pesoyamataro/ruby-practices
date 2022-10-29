7cf44628c71c2b2e784184907c8d7ddfd112f8fc
pesoyama <pesoyama.taro@gmail.com>
2022-10-29 16:26:31 +0900

    review3 fix

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -15,40 +15,39 @@ def search_files(path)
   return [path] if FileTest.file?(path.to_s)
 
   dirpath = ARGV.empty? ? Dir.getwd : path
-  files = []
-  Dir.foreach(dirpath) do |file|
-    files << file unless file.start_with?('.')
+  files = Dir.foreach(dirpath).reject do |file|
+    file.start_with?('.')
   end
   files.sort
 end
 
-def output(files_name)
-  row_count = (files_name.size.to_f / MAX_COLUMN).ceil
-  display_width = adjust_width(files_name).max
+def output(files)
+  row_count = (files.size.to_f / MAX_COLUMN).ceil
+  display_width = adjust_width(files).max
   row_count.times do |i|
     MAX_COLUMN.times do |j|
-      print ljust_kana(files_name[i + j * row_count], display_width)
+      print ljust_kana(files[i + j * row_count], display_width)
       print "\n" if ((j + 1) % MAX_COLUMN).zero?
     end
   end
 end
 
-def adjust_width(files_chars)
+def adjust_width(file_chars)
   addition_count = 2
-  files_chars.map do |files_char|
-    count_ascii(files_char, addition_count) + SPACE_LENGTH
+  file_chars.map do |file_char|
+    count_chars(file_char, addition_count) + SPACE_LENGTH
   end
 end
 
-def ljust_kana(file_name, max_width)
+def ljust_kana(file_name, display_width)
   return '' if file_name.nil?
 
   addition_count = 1
-  file_name.ljust(max_width - count_ascii(file_name, addition_count))
+  file_name.ljust(display_width - count_chars(file_name, addition_count))
 end
 
-def count_ascii(str, char_count)
-  str.chars.sum { |c| c.ascii_only? ? char_count - 1 : char_count }
+def count_chars(file_name, addition_count)
+  file_name.chars.sum { |c| c.ascii_only? ? addition_count - 1 : addition_count }
 end
 
 main
