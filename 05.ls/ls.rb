da5a7b51a5fd62d2efb5ce064a569c972e20bfc9
pesoyama <pesoyama.taro@gmail.com>
2022-12-21 16:30:10 +0900

    review1-fix

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -8,6 +8,7 @@ require 'date'
 MAX_COLUMN = 3
 SPACE_LENGTH = 3
 BLOCK_SIZE = 4096
+
 PERMISSIONS = {
   '0' => '---',
   '1' => '--x',
@@ -18,6 +19,7 @@ PERMISSIONS = {
   '6' => 'rw-',
   '7' => 'rwx'
 }.freeze
+
 FTYPE = {
   'fifo' => 'p',
   'characterSpecial' => 'c',
@@ -30,7 +32,16 @@ FTYPE = {
 
 def main
   params, argv_path = separate_argv
-  search_files(params, argv_path)
+  dirpath = argv_path || Dir.getwd
+  simplex_file = true if FileTest.file?(argv_path.to_s)
+  all_files = search_files(params, argv_path, dirpath, simplex_file)
+  if params[:l]
+    detail_array_files = get_detail_files(argv_path, all_files, dirpath, simplex_file) if params[:l]
+    detail_display_size = adjust_display_size(detail_array_files)
+    output_detail(detail_array_files, detail_display_size, simplex_file)
+  else
+    output_normal(all_files)
+  end
 end
 
 def separate_argv
@@ -42,22 +53,17 @@ def separate_argv
   [params, option.parse!(ARGV)[0]]
 end
 
-def search_files(params, argv_path)
-  if FileTest.file?(argv_path.to_s)
-    return params[:l] ? get_detail_files(File.dirname(argv_path), [File.basename(argv_path)], argv_path.to_s) : output_normal([argv_path])
+def search_files(params, argv_path, dirpath, simplex_file)
+  return [argv_path] if simplex_file
 
-  end
-
-  dirpath = argv_path || Dir.getwd
   file_names = Dir.foreach(dirpath).reject { |file| file.start_with?('.') && !params[:a] }.sort
-  rearrange_files = params[:r] ? file_names.reverse : file_names
-  params[:l] ? get_detail_files(dirpath, rearrange_files) : output_normal(rearrange_files)
+  params[:r] ? file_names.reverse : file_names
 end
 
-def get_detail_files(dirpath, file_names, simplex_file = nil)
+def get_detail_files(_argv_path, file_names, dirpath, simplex_file)
   detail_array_files = []
   file_names.map do |file_name|
-    fullpath = "#{dirpath}/#{file_name}"
+    fullpath = simplex_file ? file_name : "#{dirpath}/#{file_name}"
     file_lstat = File.lstat(fullpath)
     detail_files = {}
     detail_files[:ftype] = FTYPE[file_lstat.ftype]
@@ -69,56 +75,56 @@ def get_detail_files(dirpath, file_names, simplex_file = nil)
     detail_files[:gid] = Etc.getgrgid(file_lstat.gid).name
     detail_files[:file_size] = File.size(fullpath).to_s
     detail_files[:mtime] = file_lstat.mtime.strftime('%_m月 %_d %_R')
-    detail_files[:file_name] = simplex_file || file_name
+    detail_files[:file_name] = file_name
     detail_files[:block_size] = file_lstat.blocks / 2 # 1ブロックサイズを512byte⇒1024byteに変換
     detail_array_files << detail_files
   end
-  adjust_size(detail_array_files, simplex_file)
+  detail_array_files
 end
 
-def adjust_size(detail_array_files, simplex_file)
+def adjust_display_size(detail_array_files)
   all_nlink = []
   all_owner = []
   all_group = []
   all_file_size = []
-  total_size = 0
+  all_block_size = []
   detail_array_files.each do |detail_array_file|
     all_nlink << detail_array_file[:nlink]
     all_owner << detail_array_file[:uid]
     all_group << detail_array_file[:gid]
     all_file_size << detail_array_file[:file_size]
-    total_size += detail_array_file[:block_size].to_i if simplex_file.nil?
+    all_block_size << detail_array_file[:block_size]
   end
-  detail_space_length = {
+  {
     space_nlink: all_nlink.map(&:size).max,
     space_owner: all_owner.map(&:size).max,
     space_group: all_group.map(&:size).max,
-    space_file_size: all_file_size.map(&:size).max
+    space_file_size: all_file_size.map(&:size).max,
+    total_size: all_block_size.sum
   }
-  output_detail(detail_array_files, detail_space_length, total_size)
 end
 
-def output_detail(file_all, detail_space_length, total_size)
-  puts "合計 #{total_size}" unless total_size.zero?
+def output_detail(file_all, detail_display_size, simplex_file)
+  puts "合計 #{detail_display_size[:total_size]}" unless simplex_file
   file_all.each do |file_list|
     print file_list[:ftype]
     print "#{file_list[:permission]} "
-    print "#{file_list[:nlink].rjust(detail_space_length[:space_nlink])} "
-    print "#{file_list[:uid].ljust(detail_space_length[:space_owner])} "
-    print "#{file_list[:gid].ljust(detail_space_length[:space_group])} "
-    print "#{file_list[:file_size].rjust(detail_space_length[:space_file_size])} "
+    print "#{file_list[:nlink].rjust(detail_display_size[:space_nlink])} "
+    print "#{file_list[:uid].ljust(detail_display_size[:space_owner])} "
+    print "#{file_list[:gid].ljust(detail_display_size[:space_group])} "
+    print "#{file_list[:file_size].rjust(detail_display_size[:space_file_size])} "
     print "#{file_list[:mtime]} "
     print file_list[:file_name]
     print "\n"
   end
 end
 
-def output_normal(files)
-  row_count = (files.size.to_f / MAX_COLUMN).ceil
-  display_width = adjust_width(files).max
+def output_normal(all_files)
+  row_count = (all_files.size.to_f / MAX_COLUMN).ceil
+  display_width = adjust_width(all_files).max
   row_count.times do |i|
     MAX_COLUMN.times do |j|
-      print ljust_kana(files[i + j * row_count], display_width)
+      print ljust_kana(all_files[i + j * row_count], display_width)
       print "\n" if ((j + 1) % MAX_COLUMN).zero?
     end
   end
