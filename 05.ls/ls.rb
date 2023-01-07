d777747fec94266e8d8882c88a30e63e935a09d0
pesoyama <pesoyama.taro@gmail.com>
2023-01-07 10:36:58 +0900

    review2 fix

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -32,13 +32,12 @@ FTYPE = {
 
 def main
   params, argv_path = separate_argv
-  dirpath = argv_path || Dir.getwd
-  simplex_file = true if FileTest.file?(argv_path.to_s)
-  all_files = search_files(params, argv_path, dirpath, simplex_file)
+  dirpath, all_files = FileTest.file?(argv_path) ? [File.dirname(argv_path), [File.basename(argv_path)]] : [argv_path, search_files(params, argv_path)]
   if params[:l]
-    detail_array_files = get_detail_files(argv_path, all_files, dirpath, simplex_file) if params[:l]
-    detail_display_size = adjust_display_size(detail_array_files)
-    output_detail(detail_array_files, detail_display_size, simplex_file)
+    detail_files = get_detail_files(all_files, dirpath)
+    detail_display_size = adjust_display_size(detail_files)
+    total_size = detail_files.map { |detail_file| detail_file[:block_size] }.sum unless FileTest.file?(argv_path)
+    output_detail(detail_files, detail_display_size, total_size)
   else
     output_normal(all_files)
   end
@@ -50,26 +49,22 @@ def separate_argv
   option.on('-a') { |v| params[:a] = v }
   option.on('-r') { |v| params[:r] = v }
   option.on('-l') { |v| params[:l] = v }
-  [params, option.parse!(ARGV)[0]]
+  argv_path = option.parse!(ARGV)[0] || Dir.getwd
+  [params, argv_path]
 end
 
-def search_files(params, argv_path, dirpath, simplex_file)
-  return [argv_path] if simplex_file
-
+def search_files(params, dirpath)
   file_names = Dir.foreach(dirpath).reject { |file| file.start_with?('.') && !params[:a] }.sort
   params[:r] ? file_names.reverse : file_names
 end
 
-def get_detail_files(_argv_path, file_names, dirpath, simplex_file)
-  detail_array_files = []
+def get_detail_files(file_names, dirpath)
   file_names.map do |file_name|
-    fullpath = simplex_file ? file_name : "#{dirpath}/#{file_name}"
+    fullpath = "#{dirpath}/#{file_name}"
     file_lstat = File.lstat(fullpath)
     detail_files = {}
     detail_files[:ftype] = FTYPE[file_lstat.ftype]
-    element_permission = []
-    -3.upto(-1) { |i| element_permission << PERMISSIONS[file_lstat.mode.to_s(8).slice(i)] }
-    detail_files[:permission] = element_permission.join
+    detail_files[:permission] = -3.upto(-1).map { |i| PERMISSIONS[file_lstat.mode.to_s(8).slice(i)] }.join
     detail_files[:nlink] = file_lstat.nlink.to_s
     detail_files[:uid] = Etc.getpwuid(file_lstat.uid).name
     detail_files[:gid] = Etc.getgrgid(file_lstat.gid).name
@@ -77,44 +72,40 @@ def get_detail_files(_argv_path, file_names, dirpath, simplex_file)
     detail_files[:mtime] = file_lstat.mtime.strftime('%_m月 %_d %_R')
     detail_files[:file_name] = file_name
     detail_files[:block_size] = file_lstat.blocks / 2 # 1ブロックサイズを512byte⇒1024byteに変換
-    detail_array_files << detail_files
+    detail_files
   end
-  detail_array_files
 end
 
-def adjust_display_size(detail_array_files)
+def adjust_display_size(detail_files)
   all_nlink = []
   all_owner = []
   all_group = []
   all_file_size = []
-  all_block_size = []
-  detail_array_files.each do |detail_array_file|
-    all_nlink << detail_array_file[:nlink]
-    all_owner << detail_array_file[:uid]
-    all_group << detail_array_file[:gid]
-    all_file_size << detail_array_file[:file_size]
-    all_block_size << detail_array_file[:block_size]
+  detail_files.each do |detail_file|
+    all_nlink << detail_file[:nlink]
+    all_owner << detail_file[:uid]
+    all_group << detail_file[:gid]
+    all_file_size << detail_file[:file_size]
   end
   {
     space_nlink: all_nlink.map(&:size).max,
     space_owner: all_owner.map(&:size).max,
     space_group: all_group.map(&:size).max,
-    space_file_size: all_file_size.map(&:size).max,
-    total_size: all_block_size.sum
+    space_file_size: all_file_size.map(&:size).max
   }
 end
 
-def output_detail(file_all, detail_display_size, simplex_file)
-  puts "合計 #{detail_display_size[:total_size]}" unless simplex_file
-  file_all.each do |file_list|
-    print file_list[:ftype]
-    print "#{file_list[:permission]} "
-    print "#{file_list[:nlink].rjust(detail_display_size[:space_nlink])} "
-    print "#{file_list[:uid].ljust(detail_display_size[:space_owner])} "
-    print "#{file_list[:gid].ljust(detail_display_size[:space_group])} "
-    print "#{file_list[:file_size].rjust(detail_display_size[:space_file_size])} "
-    print "#{file_list[:mtime]} "
-    print file_list[:file_name]
+def output_detail(detail_files, detail_display_size, total_size)
+  puts "合計 #{total_size}" unless total_size.nil?
+  detail_files.each do |detail_file|
+    print detail_file[:ftype]
+    print "#{detail_file[:permission]} "
+    print "#{detail_file[:nlink].rjust(detail_display_size[:space_nlink])} "
+    print "#{detail_file[:uid].ljust(detail_display_size[:space_owner])} "
+    print "#{detail_file[:gid].ljust(detail_display_size[:space_group])} "
+    print "#{detail_file[:file_size].rjust(detail_display_size[:space_file_size])} "
+    print "#{detail_file[:mtime]} "
+    print detail_file[:file_name]
     print "\n"
   end
 end
